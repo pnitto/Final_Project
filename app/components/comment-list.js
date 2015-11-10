9a4a6b12d45b5b7e7a75b6daf78a568111a87135
Paul Nitto <pnitto18@gmail.com>
2015-11-09 23:54:46 -0500

    random styles

--- app/components/comment-list.js
+++ app/components/comment-list.js
@@ -44,7 +44,8 @@ const Chat = React.createClass({
           <li><Link to={`/chat/${x.objectId}`} state={{comment:x}}>{x.courseName}</Link>{"(" +  x.rating + " out of 5)"}<Glyphicon onClick={this.handleDelete.bind(this,x)} glyph="remove" /></li>
 
         <div className="line" />
-                  <li>{x.creator.username}:<span className="comment">{x.comment}</span></li>
+                  <li>{x.creator.username}</li>
+                    <li><span className="comment">{x.comment}</span></li>
                 </div>)
         })}
       </ul>
