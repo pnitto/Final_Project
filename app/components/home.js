2a629239be3f2e64d650fd4bb8a06fa6cdb88c0d
Paul Nitto <pnitto18@gmail.com>
2015-11-13 10:26:19 -0500

    last minute styles

--- app/components/home.js
+++ app/components/home.js
@@ -7,7 +7,7 @@ const Home = React.createClass({
   render(){
     return (
       <div className="home-div">
-        <h1>Welcome to Fore Score 2.0</h1>
+        <h1 className="welcome">Welcome to Fore Score 2.0</h1>
         <Link to="/create-scorecard" className="home-scorecard-link"><Button type="button" bsSize="large" className="btn btn-success begin-round-btn" data-toggle="button" aria-pressed="false" autoComplete="off">Begin Round</Button></Link>
       </div>
     )
